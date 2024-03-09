4198afc2c25379c6c6278907544dc7cb884c8f13
Aadit Panta <contact.ap0xf@gmail.com>
2024-03-09 05:52:21 +0545

    revising numbers in py

---
+++ numbers_in_python.py
@@ -0,0 +1,15 @@
+# floor division
+print(4//2)
+
+# normal division returns a float value
+print(4/2)
+
+print(n)
+
+# any mathematical operation which includes a float number will always give float as output
+
+
+# besides int and float, python has number types called Decimal and Fraction
+
+
+
