abc81657aeea55a5148f641c1efb809aaaa1f68e
Aadit Panta <contact.ap0xf@gmail.com>
2024-03-09 05:52:57 +0545

    concepts of mutable and immutable datatypes in python

---
+++ mutable_and_immutable.py
@@ -0,0 +1,7 @@
+lst1 = [1,2,3]
+print(lst1)
+
+
+lst2 = lst1 # this is not creating a new list called lst2 and copying the value of lst1 to lst2
+            # instead it is just creating a new pointer that points to same memory locaion as lst1 does.
+            # so ch
