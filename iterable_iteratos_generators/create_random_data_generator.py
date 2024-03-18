9275213af6e2127852d1486be8e27f5fabebc0e8
ap0xF <contact.ap0xf@gmail.com>
2024-03-18 21:53:25 +0545

    inf random data generator using generator expression synatx

--- iterable_iteratos_generators/create_random_data_generator.py
+++ iterable_iteratos_generators/create_random_data_generator.py
@@ -1,3 +1,10 @@
 import random
 
-random_data =
+random_data = (random.randint(1,11) for _ in iter(int, 1))
+
+stop_iteration_on_ten = 0
+for i in random_data:
+    if stop_iteration_on_ten < 10:
+        print(i)
+    stop_iteration_on_ten += 1
+
